6b9e054933eab4afca6656dd219ca473c71993f5
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2019-12-13 18:58:20 +0100

    Update ConverterInterface.php

--- src/ConverterInterface.php
+++ src/ConverterInterface.php
@@ -6,12 +6,12 @@ interface ConverterInterface
 {
 	/**
 	 * initial currency conversion
-     *
-     * @param  string|array $from
+         *
+         * @param  string|array $from
 	 * @param  string|array $to
 	 * @param  float 		$amount
 	 * @param  bool			$short
-     * @return string|array
-     */
+         * @return string|array
+         */
 	public function cconv($from, $to, $amount);
 }
