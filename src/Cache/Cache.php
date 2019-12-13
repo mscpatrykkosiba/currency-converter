4cfe1b66cf9287576c5c13b2dd9f71af01974948
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2019-12-13 18:59:32 +0100

    Update Cache.php

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -6,11 +6,11 @@ class Cache implements CacheInterface
 {
 	/**
 	 * change cache parameter
-     *
-     * @param  bool $is
+         *
+         * @param  bool $is
 	 * @param  int  $time
-     * @return bool
-     */
+         * @return bool
+         */
 	public static function setCache($is, $time)
 	{
 		if($is==true && is_numeric($time) && is_int($time)){
