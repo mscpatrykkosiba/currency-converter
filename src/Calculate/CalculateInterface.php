f0f48c696226903f6d0a8e220acf380f9cfeb7e3
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2019-12-13 18:59:18 +0100

    Update CalculateInterface.php

--- src/Calculate/CalculateInterface.php
+++ src/Calculate/CalculateInterface.php
@@ -5,15 +5,15 @@ namespace CConverter\Calculate;
 interface CalculateInterface
 {
 	/**
-     * currency conversion, cache check and fixer.io api connection
-     *
-     * @param  string|array   $getFrom
+         * currency conversion, cache check and fixer.io api connection
+         *
+         * @param  string|array   $getFrom
 	 * @param  string|array   $getTo
 	 * @param  float          $amount
 	 * @param  int            $short
 	 * @param  bool 		  $isCache
 	 * @param  int   		  $cacheTime
-     * @return string|array
-     */
+         * @return string|array
+         */
 	public function getValues($from, $to, $amount, $short, $isCache, $cacheTime);
 }
