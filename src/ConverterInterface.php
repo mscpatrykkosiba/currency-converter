51e3030fcae269a9ed4a86b216ef21f45dc2c59a
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2019-12-20 01:53:56 +0100

    Update ConverterInterface.php

--- src/ConverterInterface.php
+++ src/ConverterInterface.php
@@ -4,14 +4,14 @@ namespace CConverter;
 
 interface ConverterInterface
 {
-	/**
-	 * initial currency conversion
-         *
-         * @param  string|array $from
-	 * @param  string|array $to
-	 * @param  float 		$amount
-	 * @param  bool			$short
-         * @return string|array
-         */
-	public function cconv($from, $to, $amount);
+    /**
+     * initial currency conversion
+     *
+     * @param  string|array $from
+     * @param  string|array $to
+     * @param  float        $amount
+     * @param  bool         $short
+     * @return string|array
+     */
+    public function cconv($from, $to, $amount);
 }
